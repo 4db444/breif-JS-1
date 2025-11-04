bca2952f065cbd09977f1ff03a279fb5104bec6a
brahim444 <brahim77990alhyan@gmail.com>
2025-11-04 22:33:54 +0100

    finishing paginatoin in the market page

--- market.js
+++ market.js
@@ -51,24 +51,44 @@ let cards = [
     },
 ]
 
+let page_items = 9;
+let page_number = 1;
+let filtered_cards = [...cards]
+
 let cards_container = document.querySelector(".cards-container");
+let pagination_container = document.querySelector(".pagination-container")
+
+function update_page(page_num){
+    if(page_num !== page_number){
+        page_number = page_num;
+        renderCards(filtered_cards)
+    }
+}
 
 function renderCards (arr) {
-    cards_container.innerHTML = ""
+    let total_pages = Math.ceil(arr.length / page_items)
+    cards_container.innerHTML = "";
+    pagination_container.innerHTML = "";
 
     if(!arr.length){
         cards_container.innerHTML = `<img src="./img/not_found.png" alt="not_found" class="grayscale-100">`
     }else {
-        arr.forEach(elem => {
+        for(let i = page_items * (page_number - 1); i < page_items * page_number && i < arr.length; i++){
             cards_container.innerHTML += `
-            <div class="card">
-               <div class="card-rarity-container bg-(--${elem.rarity}-card-color)">
-                    <img src="./img/rarity/${elem.rarity} tag.png" alt="" class="card-rarity">
+                <div class="card">
+                    <div class="card-rarity-container bg-(--${arr[i].rarity}-card-color)">
+                        <img src="./img/rarity/${arr[i].rarity} tag.png" alt="" class="card-rarity">
+                    </div>
+                    <img src="${arr[i].img}" alt="" class="card-img border-[20px]  border-(--${arr[i].rarity}-card-color)">
                 </div>
-                <img src="${elem.img}" alt="" class="card-img border-[20px]  border-(--${elem.rarity}-card-color)">
-            </div>
             `
-        });
+        }
+
+        for(let i = 1; i <= total_pages; i++){
+            pagination_container.innerHTML += `
+                <button onclick="update_page(${i})" class="w-[50px] h-[50px] border-3 border-(--main-red) text-(--main-red) text-xl font-bold rounded-[10px] ${i == page_number ? "bg-(--main-red) text-white" : ""}">${i}</button>
+            `
+        }
     }
 }
 
@@ -77,11 +97,14 @@ renderCards(cards)
 
 document.querySelector("form").addEventListener("change", (e)=> {
     let filter_by = e.target.id
+    page_number = 1;
 
     if (filter_by === "all"){
-        renderCards(cards)
+        filtered_cards = [...cards]
+        renderCards(filtered_cards)
     }else {
-        renderCards(cards.filter(elem => elem.rarity === filter_by))
+        filtered_cards = cards.filter(elem => elem.rarity === filter_by)
+        renderCards(filtered_cards)
     }
 
 })
