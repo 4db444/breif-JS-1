60a9eeaf9cd3744c245840aa856c154bd43aa282
brahim444 <brahim77990alhyan@gmail.com>
2025-11-05 09:19:23 +0100

    add description and small animation for cards

--- market.js
+++ market.js
@@ -2,52 +2,62 @@ let cards = [
     {
         name : "Roger",
         img : "./img/cards/roger.png",
-        rarity : "pirate-king"
+        rarity : "pirate-king",
+        is_liked : false
     },
     {
         name : "Nika",
         img : "./img/cards/nika.png",
-        rarity : "yonko"
+        rarity : "yonko",
+        is_liked : false
     },
     {
         name : "White beard",
         img : "./img/cards/oyaji.png",
-        rarity : "pirate-king"
+        rarity : "pirate-king",
+        is_liked : false
     },
     {
         name : "Shanks",
         img : "./img/cards/shanks.png",
-        rarity : "yonko"
+        rarity : "yonko",
+        is_liked : false
     },
     {
         name : "Choper",
         img : "./img/cards/choper.png",
-        rarity : "marin"
+        rarity : "marin",
+        is_liked : false
     },
     {
         name : "Zoro",
         img : "./img/cards/zoro.png",
-        rarity : "pirate"
+        rarity : "pirate",
+        is_liked : false
     },
     {
         name : "Kizaru",
         img : "./img/cards/Kizaru.png",
-        rarity : "yonko"
+        rarity : "yonko",
+        is_liked : false
     },
     {
         name : "Luffy",
         img : "./img/cards/luffy.png",
-        rarity : "pirate"
+        rarity : "pirate",
+        is_liked : false
     },
     {
         name : "Law",
         img : "./img/cards/law.png",
-        rarity : "pirate"
+        rarity : "pirate",
+        is_liked : false
     },
     {
         name : "Bepo",
         img : "./img/cards/bepo.png",
-        rarity : "marin"
+        rarity : "marin",
+        is_liked : false
     },
 ]
 
@@ -65,6 +75,10 @@ function update_page(page_num){
     }
 }
 
+function toggleLike (){
+    return
+}
+
 function renderCards (arr) {
     let total_pages = Math.ceil(arr.length / page_items)
     cards_container.innerHTML = "";
@@ -75,11 +89,16 @@ function renderCards (arr) {
     }else {
         for(let i = page_items * (page_number - 1); i < page_items * page_number && i < arr.length; i++){
             cards_container.innerHTML += `
-                <div class="card">
+                <div class="card group">
+                    <img src="${arr[i].img}" alt="" class="card-img border-[20px]  border-(--${arr[i].rarity}-card-color) hover:brightness-70 transition">
                     <div class="card-rarity-container bg-(--${arr[i].rarity}-card-color)">
                         <img src="./img/rarity/${arr[i].rarity} tag.png" alt="" class="card-rarity">
                     </div>
-                    <img src="${arr[i].img}" alt="" class="card-img border-[20px]  border-(--${arr[i].rarity}-card-color)">
+
+                    <button class="text-white w-[100px] h-[100px] bg-(--${arr[i].rarity}-card-color) rounded-full absolute top-[50%] left-0 ml-[20px] translate-y-[-50%] translate-x-[calc(-100%-20px)] group-hover:translate-x-0 transition" onclick="toggleLike()">like</button>
+                    <button class="text-white w-[100px] h-[100px] bg-(--${arr[i].rarity}-card-color) rounded-full absolute top-[50%] right-0 mr-[20px] translate-y-[-50%] translate-x-[calc(100%+20px)] group-hover:translate-x-0 transition">cart</button>
+                    <div ></div>
+                    <div class="w-full h-[100px] bg-(--${arr[i].rarity}-card-color) absolute bottom-0 translate-y-[100%] group-hover:translate-y-0 transition"></div>
                 </div>
             `
         }
