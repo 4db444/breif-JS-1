088af5c9d897e7b9a0b4be979cbae8da53b0cbbb
brahim444 <brahim77990alhyan@gmail.com>
2025-11-06 18:13:28 +0100

    finishing the clear functionality in cart

--- cart.js
+++ cart.js
@@ -1,6 +1,8 @@
 const cart_container = document.querySelector(".cart-container")
 const cart_list = document.querySelector(".cart-list")
 const cart_btn = document.querySelector(".cart-btn")
+const order_price = document.querySelector(".order-price")
+const clear_btn= document.querySelector(".clear-cart-btn")
 let cart_items = JSON.parse(localStorage.getItem("cart"));
 let time_out = null;
 
@@ -17,8 +19,22 @@ cart_btn.addEventListener("click", e => {
     }
 })
 
+function update_order_price (){
+    order_price.textContent = `Order Price: ${cart_items.reduce((acc, next) => acc + next.price * next.quantity, 0)}$`
+}
+
 cart_container.addEventListener("blur", ()=> {
     cart_container.classList.add("translate-x-[100%]")
+})
+
+clear_btn.addEventListener("click", (e) => {
+    
+    cart_items.forEach(elem => {
+        document.getElementById(`btn-cart-${elem.id}`).innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 5L19 12H7.37671M20 16H8L6 3H3M16 5.5H13.5M13.5 5.5H11M13.5 5.5V8M13.5 5.5V3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
+    })
+    cart_items = [];
+    localStorage.setItem("cart", "[]")
+    render_cart(cart_items)
 })
 
 window.addEventListener("scroll", (e) => {
@@ -27,11 +43,13 @@ window.addEventListener("scroll", (e) => {
 
 function increase_quantity (id){
     const cart_item = cart_items.find(elem => elem.id === id);
+    console.log(cart_item)
     cart_item.quantity += 1;
 
     document.getElementById(`span-quantity-${id}`).textContent = cart_item.quantity;
     document.getElementById(`total-${id}`).textContent = `total : ${cart_item.quantity * cart_item.price}$`;
 
+    update_order_price()
     localStorage.setItem("cart", JSON.stringify(cart_items))
 }
 
@@ -44,18 +62,22 @@ function decrease_quantity (id){
         document.getElementById(`total-${id}`).textContent = `total : ${cart_item.quantity * cart_item.price}$`;
     }else {
         cart_items = cart_items.filter(elem => elem.id !== id)
+        // const cards = JSON.parse(localStorage.getItem("cards"))
         cards.find(elem => elem.id === id).in_cart = false;
+        localStorage.setItem("cards", JSON.stringify(cards))
         render_cart(cart_items)
         
         document.getElementById(`btn-cart-${id}`).innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 5L19 12H7.37671M20 16H8L6 3H3M16 5.5H13.5M13.5 5.5H11M13.5 5.5V8M13.5 5.5V3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
         
-        clearTimeout(time_out);
-        time_out = setTimeout(() => {
-            cart_container.classList.add("translate-x-[100%]")
-        }, 1000);
-        
-        localStorage.setItem("cards", JSON.stringify(cards))
+        if(!cart_items.length){
+            clearTimeout(time_out);
+            time_out = setTimeout(() => {
+                cart_container.classList.add("translate-x-[100%]")
+            }, 1000);
+        }
     }
+
+    update_order_price()
     
     localStorage.setItem("cart", JSON.stringify(cart_items))
 }
@@ -82,6 +104,8 @@ function render_cart (arr) {
     }else {
         cart_list.innerHTML = `<img src="./img/sleeping_zoro.png" alt="hi" class="grayscale-100"/>`
     }
+
+    update_order_price()
 }
 
 render_cart(cart_items)
--- market.js
+++ market.js
@@ -1,5 +1,5 @@
 let cards = JSON.parse(localStorage.getItem("cards"))
-let cart = JSON.parse(localStorage.getItem("cart"))
+// let cart = JSON.parse(localStorage.getItem("cart"))
 
 let page_items = 9;
 let page_number = 1;
@@ -28,19 +28,22 @@ function toggleLike (id){
 }
 
 function toggleCart (id) {
-    const card_to_update = filtered_cards.find(elem => elem.id === id)
+    cards = JSON.parse(localStorage.getItem("cards"))
+    const card_to_update = cards.find(elem => elem.id === id)
 
-    cart = JSON.parse(localStorage.getItem("cart"))
+    // cart_items = JSON.parse(localStorage.getItem("cart"))
     if(card_to_update.in_cart){
-        cart = cart.filter(elem => elem.id !== id)
+        cart_items = cart_items.filter(elem => elem.id !== id)
+        console.log(card_to_update)
     }else {
-        cart.push({
+        console.log("push")
+        cart_items.push({
             ...card_to_update,
             quantity : 1
         })
     }
     
-    render_cart(cart)
+    render_cart(cart_items)
     card_to_update.in_cart = !card_to_update.in_cart;
 
     document.getElementById(`btn-cart-${id}`).innerHTML = card_to_update.in_cart ? 
@@ -49,7 +52,7 @@ function toggleCart (id) {
                             `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 5L19 12H7.37671M20 16H8L6 3H3M16 5.5H13.5M13.5 5.5H11M13.5 5.5V8M13.5 5.5V3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
 
     localStorage.setItem("cards", JSON.stringify(cards))
-    localStorage.setItem("cart", JSON.stringify(cart))
+    localStorage.setItem("cart", JSON.stringify(cart_items))
 }
 
 function renderCards (arr) {
