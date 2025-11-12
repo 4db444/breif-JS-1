f2cbe679f824ed654a11ee2085d26703ad88ebbd
brahim444 <brahim77990alhyan@gmail.com>
2025-11-12 15:06:32 +0100

    add card position (attack/defence)

--- localStorage.js
+++ localStorage.js
@@ -1,104 +1,124 @@
 const initialData = [
-    {
-        id: 1,
-        name: "Roger",
-        img: "./img/cards/roger.png",
-        rarity: "pirate-king",
-        is_liked: false,
-        in_cart: false,
-        price: 138,
-        description: "Legendary Pirate King who discovered the One Piece and inspired a new era of pirates."
-    },
-    {
-        id: 2,
-        name: "Nika",
-        img: "./img/cards/nika.png",
-        rarity: "yonko",
-        is_liked: false,
-        in_cart: false,
-        price: 120,
-        description: "Mythical Sun God of freedom, symbolizing joy and liberation for all oppressed people."
-    },
-    {
-        id: 3,
-        name: "White beard",
-        img: "./img/cards/oyaji.png",
-        rarity: "pirate-king",
-        is_liked: false,
-        in_cart: false,
-        price: 144,
-        description: "Mighty pirate known as the Strongest Man Alive and protector of his vast family."
-    },
-    {
-        id: 4,
-        name: "Shanks",
-        img: "./img/cards/shanks.png",
-        rarity: "yonko",
-        is_liked: false,
-        in_cart: false,
-        price: 110,
-        description: "Charismatic Yonko who values peace, respect, and the balance of the seas."
-    },
-    {
-        id: 5,
-        name: "Choper",
-        img: "./img/cards/choper.png",
-        rarity: "marin",
-        is_liked: false,
-        in_cart: false,
-        price: 56,
-        description: "Adorable reindeer doctor with a big heart and a love for his crew."
-    },
-    {
-        id: 6,
-        name: "Zoro",
-        img: "./img/cards/zoro.png",
-        rarity: "pirate",
-        is_liked: false,
-        in_cart: false,
-        price: 98,
-        description: "Fearless swordsman striving to become the world’s greatest master of the blade."
-    },
-    {
-        id: 7,
-        name: "Kizaru",
-        img: "./img/cards/Kizaru.png",
-        rarity: "yonko",
-        is_liked: false,
-        in_cart: false,
-        price: 85,
-        description: "Laid-back admiral with the power of light, moving at the speed of brilliance."
-    },
-    {
-        id: 8,
-        name: "Luffy",
-        img: "./img/cards/luffy.png",
-        rarity: "pirate",
-        is_liked: false,
-        in_cart: false,
-        price: 130,
-        description: "Cheerful pirate captain dreaming of freedom and adventure as the next Pirate King."
-    },
-    {
-        id: 9,
-        name: "Law",
-        img: "./img/cards/law.png",
-        rarity: "pirate",
-        is_liked: false,
-        in_cart: false,
-        price: 77,
-        description: "Tactical pirate surgeon with the power to manipulate space through the Ope Ope no Mi."
-    },
-    {
-        id: 10,
-        name: "Bepo",
-        img: "./img/cards/bepo.png",
-        rarity: "marin",
-        is_liked: false,
-        in_cart: false,
-        price: 49,
-        description: "Loyal polar bear mink and crewmate of Law, known for his courage and kindness."
-    },
+  {
+    id: 1,
+    name: "Roger",
+    img: "./img/cards/roger.png",
+    rarity: "pirate-king",
+    is_liked: false,
+    in_cart: false,
+    price: 138,
+    description: "Legendary Pirate King who discovered the One Piece and inspired a new era of pirates.",
+    attack: 9500,
+    shield: 8800
+  },
+  {
+    id: 2,
+    name: "Nika",
+    img: "./img/cards/nika.png",
+    rarity: "yonko",
+    is_liked: false,
+    in_cart: false,
+    price: 120,
+    description: "Mythical Sun God of freedom, symbolizing joy and liberation for all oppressed people.",
+    attack: 9700,
+    shield: 8600
+  },
+  {
+    id: 3,
+    name: "White beard",
+    img: "./img/cards/oyaji.png",
+    rarity: "pirate-king",
+    is_liked: false,
+    in_cart: false,
+    price: 144,
+    description: "Mighty pirate known as the Strongest Man Alive and protector of his vast family.",
+    attack: 9800,
+    shield: 9000
+  },
+  {
+    id: 4,
+    name: "Shanks",
+    img: "./img/cards/shanks.png",
+    rarity: "yonko",
+    is_liked: false,
+    in_cart: false,
+    price: 110,
+    description: "Charismatic Yonko who values peace, respect, and the balance of the seas.",
+    attack: 9000,
+    shield: 8900
+  },
+  {
+    id: 5,
+    name: "Choper",
+    img: "./img/cards/choper.png",
+    rarity: "marin",
+    is_liked: false,
+    in_cart: false,
+    price: 56,
+    description: "Adorable reindeer doctor with a big heart and a love for his crew.",
+    attack: 3100,
+    shield: 5000
+  },
+  {
+    id: 6,
+    name: "Zoro",
+    img: "./img/cards/zoro.png",
+    rarity: "pirate",
+    is_liked: false,
+    in_cart: false,
+    price: 98,
+    description: "Fearless swordsman striving to become the world’s greatest master of the blade.",
+    attack: 8800,
+    shield: 8200
+  },
+  {
+    id: 7,
+    name: "Kizaru",
+    img: "./img/cards/Kizaru.png",
+    rarity: "yonko",
+    is_liked: false,
+    in_cart: false,
+    price: 85,
+    description: "Laid-back admiral with the power of light, moving at the speed of brilliance.",
+    attack: 8900,
+    shield: 8700
+  },
+  {
+    id: 8,
+    name: "Luffy",
+    img: "./img/cards/luffy.png",
+    rarity: "pirate",
+    is_liked: false,
+    in_cart: false,
+    price: 130,
+    description: "Cheerful pirate captain dreaming of freedom and adventure as the next Pirate King.",
+    attack: 9300,
+    shield: 8500
+  },
+  {
+    id: 9,
+    name: "Law",
+    img: "./img/cards/law.png",
+    rarity: "pirate",
+    is_liked: false,
+    in_cart: false,
+    price: 77,
+    description: "Tactical pirate surgeon with the power to manipulate space through the Ope Ope no Mi.",
+    attack: 8400,
+    shield: 8100
+  },
+  {
+    id: 10,
+    name: "Bepo",
+    img: "./img/cards/bepo.png",
+    rarity: "marin",
+    is_liked: false,
+    in_cart: false,
+    price: 49,
+    description: "Loyal polar bear mink and crewmate of Law, known for his courage and kindness.",
+    attack: 4200,
+    shield: 5800
+  }
 ];
 
 if(!localStorage.getItem("cards")) localStorage.setItem("cards", JSON.stringify(initialData));
--- play.js
+++ play.js
@@ -3,8 +3,8 @@ const hand = document.querySelector(".hand");
 const playerArena = document.querySelector(".player-arena")
 
 const my_deck = JSON.parse(localStorage.getItem("myDeck"))
+const cardsToPlay = [];7
 
-const cardsToPlay = [];
 
 my_deck.forEach(elem => {
     for(let i = 0; i < elem.quantity; i++){
@@ -27,15 +27,19 @@ shuffle(cardsToPlay)
 
 cardsToPlay.forEach(elem => {
     left_side_bar.innerHTML += `
-        <div class="card deck-card draggable absolute top-0 left-0 w-full perspective-midrange transition" draggable="true">
-            <div class="wrapper transform-3d transition-transform duration-500 rotate-y-180 relative w-full h-full">
-                <div class="front absolute top-0 left-0 w-full h-full backface-hidden">
+        <div class="card deck-card draggable absolute top-0 left-0 w-full perspective-midrange transition overflow-hidden" draggable="true">
+            <div class="wrapper transform-3d transition-transform duration-500 relative w-full h-full rotate-y-180 ">
+                <div class="front absolute top-0 left-0 w-full h-full backface-hidden overflow-hidden">
                     <img src="${elem.img}" alt="" class="card-img border-[10px] border-(--${elem.rarity}-card-color) w-full h-full">
                     <div class="card-rarity-container bg-(--${elem.rarity}-card-color)">
                         <img src="./img/rarity/${elem.rarity} tag.png" alt="" class="card-rarity">
                     </div>
+                    <div class="absolute bottom-0 w-full h-[60px] flex justify-around items-center p-2 text-white bg-(--${elem.rarity}-card-color)">
+                        <h3 class="shield">${elem.shield} <i class="fa-solid fa-shield-halved"></i></h3>
+                        <h3 class="attack">${elem.attack} <i class="fa-solid fa-fire"></i></h3>
+                    </div>
                 </div>
-                <div class="absolute top-0 left-0 w-full h-full bg-white flex justify-center items-center backface-hidden rotate-y-180 border-[gray] border-[10px] rounded-[40px]">
+                <div class="absolute top-0 left-0 w-full h-full bg-white flex justify-center items-center backface-hidden rotate-y-180 border-[gray] border-[10px] rounded-[30px]">
                     <img src="./img/logo.png" class="w-[60%] grayscale-100"/>
                 </div>
             </div>
@@ -48,10 +52,8 @@ document.querySelectorAll(".draggable").forEach(elem => {
     elem.addEventListener("dragstart", (e) => {
         if(hand.children.length >= 5 && !elem.classList.contains("hand-card")){
             e.preventDefault()
-            console.log("you can not start dragging")
             return
         }
-        console.log("dragstart")
         elem.classList.add("opacity-[.5]", "dragging")
     })
     elem.addEventListener("dragend", () => {
@@ -62,8 +64,7 @@ document.querySelectorAll(".draggable").forEach(elem => {
 hand.addEventListener("dragover", (e) => {
     e.preventDefault()
     const draggalbeElement = document.querySelector(".dragging")
-    if(hand.children.length >= 5 && !draggalbeElement.classList.contains("hand-card")) return ;
-    
+    if((hand.children.length >= 5 && !draggalbeElement.classList.contains("hand-card")) || draggalbeElement.classList.contains("arena-card")) return ;
     
     const afterElement = getAfterElement(hand, e.clientX)
     draggalbeElement.classList.remove("absolute", "w-full")
@@ -74,17 +75,15 @@ hand.addEventListener("dragover", (e) => {
     }else{
         hand.appendChild(draggalbeElement)
     }
-
-    setTimeout(() => {
-    }, 300);
 })
 
 hand.addEventListener("drop", (e) => {
     const draggableElement = document.querySelector(".dragging");
     if(draggableElement.classList.contains("hand-card")) return;
-        draggableElement.classList.add("hand-card")
-        cardWrapper = draggableElement.querySelector(".wrapper")
-        cardWrapper.classList.remove("rotate-y-180")
+
+    draggableElement.classList.add("hand-card")
+    cardWrapper = draggableElement.querySelector(".wrapper")
+    cardWrapper.classList.remove("rotate-y-180")
 })
 
 function getAfterElement (container, x){
@@ -110,7 +109,7 @@ function getAfterElement (container, x){
 playerArena.querySelectorAll(".slot").forEach(elem => {
     elem.addEventListener("dragover", (e) => {
         const draggable = document.querySelector(".dragging")
-        if(draggable.classList.contains("hand-card") && !elem.children.length){
+        if((draggable.classList.contains("hand-card") || draggable.classList.contains("arena-card")) && !elem.children.length){
             e.preventDefault();
             elem.classList.add("scale-[1.1]")
         }
@@ -122,9 +121,42 @@ playerArena.querySelectorAll(".slot").forEach(elem => {
     
     elem.addEventListener("drop", e => {
         const draggable = document.querySelector(".dragging")
-        if(draggable.classList.contains("hand-card") && !elem.children.length){
+        if((draggable.classList.contains("hand-card") || draggable.classList.contains("arena-card")) && !elem.children.length){
             elem.appendChild(draggable)
-            draggable.classList.add("w-full")
+            draggable.classList.add("w-full", "arena-card")
+            
+            if(draggable.classList.contains("hand-card")) {
+                draggable.classList.remove("hand-card")
+                
+                const position_form = document.createElement("form")
+                position_form.className = "fixed w-full h-full top-0 left-0 bg-black flex flex-col gap-5 justify-center items-center";
+                position_form.addEventListener("submit", e => e.preventDefault())
+
+                const card_clone = draggable.cloneNode(true)
+                card_clone.classList.remove("opacity-[.5]", "h-full")
+
+                position_form.appendChild(card_clone);
+
+                position_form.innerHTML += `
+                    <div class="flex gap-5">
+                        <button class="defence text-white p-2 rounded-[10px] bg-green-500 hover:[transform:scale(1.1)] transition"> <i class="fa-solid fa-shield-halved"></i> Defence</button>
+                        <button class="attack text-white p-2 rounded-[10px] bg-red-500  hover:[transform:scale(1.1)] transition"> <i class="fa-solid fa-fire"></i> Attack</button>
+                    </div>
+                `
+
+                position_form.querySelector("button.defence").addEventListener("click", e => {
+                    console.log("defence card")
+                    draggable.querySelector("h3.attack").remove();
+                    position_form.remove();
+                })
+                position_form.querySelector("button.attack").addEventListener("click", e => {
+                    console.log("attack card")
+                    draggable.querySelector("h3.shield").remove();
+                    position_form.remove();
+                })
+
+                document.body.appendChild(position_form)
+            };
         }
         elem.classList.remove("scale-[1.1]")
     })
